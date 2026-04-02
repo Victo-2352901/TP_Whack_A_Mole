95e0a1de4e37faf22b0160da3c279148ab675842
RamsayAnthony <2352901@carrefour.cegepvicto.ca>
2026-04-01 20:53:58 -0400

    Continuation

--- Assets/Scripts/Controleurs/ControleurJeu.cs
+++ Assets/Scripts/Controleurs/ControleurJeu.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ControleurJeu : MonoBehaviour
@@ -9,6 +10,16 @@ public class ControleurJeu : MonoBehaviour
     private int points = 0;
     private float temps = 60f;
 
+    [SerializeField]
+    private GameObject[] spawner;
+
+    [SerializeField]
+    private GameObject ciblePrefab;
+
+    private float timerSpawn = 0f;
+    public float intervalleSpawn = 2f;
+
+    private GameObject cibleActuelle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,13 +34,23 @@ public class ControleurJeu : MonoBehaviour
             temps -= Time.deltaTime;
             ControleurUI.Instance.ActualiserTemps(temps);
 
+            timerSpawn -= Time.deltaTime;
+
+            if (timerSpawn <= 0f)
+            {
+                SpawnCible();
+                timerSpawn = intervalleSpawn;
+            }
+
+
+
             if (temps < 0)
             {
-                Fin();    
+                Fin();
             }
-        }   
+        }
     }
-    
+
     public void AjouterPoint()
     {
         points += 10;
@@ -56,5 +77,28 @@ public class ControleurJeu : MonoBehaviour
         ControleurUI.Instance.CacherStats();
         ControleurUI.Instance.AfficherMenuFin(points);
 
+    }
+    void SpawnCible()
+    {
+        if (cibleActuelle == null)
+        {
+            int index = Random.Range(0, spawner.Length);
+            Transform pointSpawn = spawner[index].transform;
+
+            cibleActuelle = Instantiate(ciblePrefab, pointSpawn.position, pointSpawn.rotation);
+
+            StartCoroutine(AutoDetruire(cibleActuelle));
+        };
+    }
+
+    public IEnumerator AutoDetruire(GameObject cible)
+    {
+        yield return new WaitForSeconds(1);
+
+        if (cible != null)
+        {
+            Destroy(cible);
+            cibleActuelle = null;
+        }
     }
 }
